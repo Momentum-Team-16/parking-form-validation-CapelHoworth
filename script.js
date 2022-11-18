e4873f6d6b09914bce44953f7866ccad69e2acfa
CapelHoworth <capelhoworth@gmail.com>
2022-11-18 13:56:22 -0500

    completed step 4

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Add validation!');
+console.log("Add validation!");
+
+const parkingForm = document.querySelector("#container");
+
+parkingForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let days = document.querySelector("#days");
+  let dayRate = 5;
+  function resCost(total, dayRate) {
+    let total = days * dayRate;
+  }
+});
